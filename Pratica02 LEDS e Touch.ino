3e01211052315584c9e2c135625c1917db4432fb
Pedro <113944862+callme-ph@users.noreply.github.com>
2023-10-26 18:03:14 -0300

    Update Pratica02 LEDS e Touch.ino
    Retirando comentários não necessários.
    Movendo o aviso de comecem e o delay que estavam dentro do loop while, o aviso estava repetindo e o delay estava atrasando o loop sem necessidade.

--- Pratica02 LEDS e Touch.ino
+++ Pratica02 LEDS e Touch.ino
@@ -1,18 +1,14 @@
 #define LED_PIN1 26
 #define LED_PIN2 25
 #define LED_PIN3 32
-#define BLINK_INTERVAL 250
-
-
 
 int ledState= LOW;
 
 unsigned long previousMillis = 0;
 void setup() {
-  // put your setup code here, to run once:
+  
   Serial.begin(115200);
-  //delay(1000);
- // Serial.println("ESP32 Touch Test");
+  
   pinMode(LED_PIN1,OUTPUT);
   pinMode(LED_PIN2,OUTPUT);
   pinMode(LED_PIN3,OUTPUT);
@@ -28,9 +24,8 @@ void loop() {
   digitalWrite(LED_PIN2, LOW);
   digitalWrite(LED_PIN3, LOW);
   
-  // put your main code here, to run repeatedly:
   unsigned long previousMillis = 0;
-  //Serial.println(touchRead(13));
+  
   if (touchRead(13)<50){
     digitalWrite(LED_PIN2, HIGH);
     delay(1000);
@@ -46,15 +41,19 @@ void loop() {
     delay (1000);
     digitalWrite(LED_PIN2, HIGH);
     unsigned long currentMillis = millis();
+    
+    Serial.println("Comecem!");
+    
      while ((touchRead(12))>65 && (touchRead(14))>65){
-        Serial.println("Comecem!");
-        delay (1000);
+       
      }
         
         if ((touchRead(12))<65){
             digitalWrite(LED_PIN3, HIGH);
             digitalWrite(LED_PIN2, LOW);
-            Serial.println(currentMillis);
+            Serial.print("Jogador 1 ganhou!. Tempo: ");
+            Serial.print(currentMillis/1000);
+            Serial.println("s");
             delay(3000);
             return; 
             
@@ -63,7 +62,9 @@ void loop() {
         if ((touchRead(14))<65){
             digitalWrite(LED_PIN1, HIGH);
             digitalWrite(LED_PIN2, LOW);
-            Serial.println(currentMillis);
+            Serial.print("Jogador 2 ganhou!. Tempo: ");
+            Serial.print(currentMillis/1000);
+            Serial.println("s");
             delay(3000);
             return; 
         }
