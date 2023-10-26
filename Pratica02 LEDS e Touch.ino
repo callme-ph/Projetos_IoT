61bb7d8268fc8f4168bef05deee0527cb2e432a2
Pedro <113944862+callme-ph@users.noreply.github.com>
2023-10-26 18:20:43 -0300

    Update Pratica02 LEDS e Touch.ino

--- Pratica02 LEDS e Touch.ino
+++ Pratica02 LEDS e Touch.ino
@@ -12,17 +12,17 @@ void setup() {
   pinMode(LED_PIN1,OUTPUT);
   pinMode(LED_PIN2,OUTPUT);
   pinMode(LED_PIN3,OUTPUT);
-  delay(3000);
   digitalWrite(LED_PIN1, LOW);
   digitalWrite(LED_PIN2, LOW);
   digitalWrite(LED_PIN3, LOW);
-  Serial.print("Aperte para iniciar");
 }
 
 void loop() {
   digitalWrite(LED_PIN1, LOW);
   digitalWrite(LED_PIN2, LOW);
   digitalWrite(LED_PIN3, LOW);
+
+  Serial.print("Aperte para iniciar");
   
   if (touchRead(13)<50){
     digitalWrite(LED_PIN2, HIGH);
