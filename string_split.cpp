48f5c3fe2e63f8bcb3098fb82781f336738e7b26
Pedro <113944862+callme-ph@users.noreply.github.com>
2023-11-24 10:23:47 -0300

    Update string_split.cpp
    Código exemplo para separação de dados concatenados em uma string.

--- string_split.cpp
+++ string_split.cpp
@@ -1,11 +1,26 @@
+/***************************
+
+- Função atof
+https://www.ibm.com/docs/en/i/7.1?topic=functions-atof-convert-character-string-float
+- Função sprintf
+https://stackoverflow.com/questions/2889421/how-to-copy-a-string-into-a-char-array-in-c-without-going-over-the-buffer 
+
+****************************/
+
+
 #include <iostream>
 #include <string>
 #include <string.h>
+#include <vector>
+#include <iterator>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
+    vector<float*> strings;
+
     // s_str é a string com os dados a serem separados.
     string s_str = "11.00-22.00-33.00-44.00-55-"; // A STRING TEM QUE TERMINAR COM UM HÍFEN
     // O último valor tem que ter o mesmo formato: 00.00
@@ -20,7 +35,7 @@ int main()
             break;
         }
         cout << "i: " << i << " pos: " << pos << endl;
-        
+
         /*strcpy(value, (s_str.substr(i, pos)).c_str());
         cout << "value: " << value << endl;
         i = pos+1;
@@ -30,6 +45,13 @@ int main()
         sprintf(value, "%.5s", (s_str.substr(i, pos)).c_str());
         cout << "value sprintf: " << value << endl;
         i = pos+1;
+
+        float result = atof(value);
+        cout << "result: " << result << endl;;
+
+        /*strings.push_back(result);
+        copy(strings.begin(),strings.end(),ostream_iterator<string>(cout," "));
+        cout << endl;*/
 
         /*char test[5];
         std::string str( "Hello, world" );
